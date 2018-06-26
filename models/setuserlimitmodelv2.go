00648652afc674b6e3c60ddb396438bea9b767aa
rucsi <rucsi6@gmail.com>
2018-06-26 11:18:15 -0400

    SetUserLimitModelV2 fix, nil time check

--- models/setuserlimitmodelv2.go
+++ models/setuserlimitmodelv2.go
@@ -1,10 +1,10 @@
 package models
 
 type SetUserLimitModelV2 struct {
-	UserID      *int64         `json:"UserId,omitempty"`        // The User ID for the limit
-	Type        *LimitType     `json:"Type,omitempty"`          // The Limit Type
-	Duration    *LimitDuration `json:"LimitDuration,omitempty"` // The Limit Duration
-	LimitAmount *float64       `json:"LimitAmount,omitempty"`   // The Limit Amount
-	StartDate   *IGCTime       `json:"StartDate,omitempty"`     // By Default, Current UTC datetime is limit start date can be set for future as well
-	EndDate     *IGCTime       `json:"EndDate,omitempty"`       // for a non-recurrent limit, End date is required, NULL for recurrent limits
+	UserID      *int64         `json:"UserId,omitempty"`      // The User ID for the limit
+	Type        *LimitType     `json:"Type,omitempty"`        // The Limit Type
+	Duration    *LimitDuration `json:"Duration,omitempty"`    // The Limit Duration
+	LimitAmount *float64       `json:"LimitAmount,omitempty"` // The Limit Amount
+	StartDate   *IGCTime       `json:"StartDate,omitempty"`   // By Default, Current UTC datetime is limit start date can be set for future as well
+	EndDate     *IGCTime       `json:"EndDate,omitempty"`     // for a non-recurrent limit, End date is required, NULL for recurrent limits
 }
