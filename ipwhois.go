f56735f20f3f2c29c9bc90be0df8ae402e91289d
rucsi <rucsi6@gmail.com>
2018-07-03 22:22:24 -0400

    IPWhoisService

--- ipwhois.go
+++ ipwhois.go
@@ -4,6 +4,8 @@ import (
 	"github.com/moonwalker/igcclient/models"
 )
 
+type IPWhoisService service
+
 // Get the country object from the IP passed in the IPWhoisRequest object
 func (s *IPWhoisService) IPWhois(body models.IPWhoisRequest, headers map[string]string) (response models.OperationResponseOfCountriesObject, err error) {
 	err = s.client.apiPost("/IPWhois", &body, &response, &headers)
