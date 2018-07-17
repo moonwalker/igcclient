a867ce7c03faca2e697fe551221bdf4fe9e609ac
Napoleon Wulkan <napoleon@wulkan.xyz>
2018-07-17 17:11:49 +0200

    fix wrong login url

--- authentication.go
+++ authentication.go
@@ -10,7 +10,7 @@ type AuthenticationService service
 
 // Used to log-in an end user to the system
 func (s *AuthenticationService) Login(loginModel models.LoginModel, headers map[string]string) (response models.OperationResponseOfAuthResponseDTO, err error) {
-	err = s.client.apiPost("v2/authentication/login", &loginModel, &response, &headers)
+	err = s.client.apiPost("/v2/authentication/login", &loginModel, &response, &headers)
 	return
 }
 
