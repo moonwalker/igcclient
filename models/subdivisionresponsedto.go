3df6aaa237a8a29118a32ef32c670b8781311181
Napoleon Wulkan <napoleon@wulkan.xyz>
2018-12-03 15:36:43 +0100

    int64 should be string

--- models/subdivisionresponsedto.go
+++ models/subdivisionresponsedto.go
@@ -1,7 +1,7 @@
 package models
 
 type SubdivisionResponseDTO struct {
-	SubdivisionId       *int64  `json:"SubdivisionId,omitempty"`
+	SubdivisionId       *string  `json:"SubdivisionId,omitempty"`
 	IsoCodeSubdivision1 *string `json:"IsoCodeSubdivision1,omitempty"`
 	IsoNameSubdivision1 *string `json:"IsoNameSubdivision1,omitempty"`
 	IsoCodeSubdivision2 *string `json:"IsoCodeSubdivision2,omitempty"`
