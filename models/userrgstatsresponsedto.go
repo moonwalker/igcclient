bd7a8957cc96da4d78e9ce430a382e1d6836b68f
Napoleon Wulkan <napoleon@wulkan.xyz>
2019-02-26 13:56:30 +0100

    fix wrong json name in model

--- models/userrgstatsresponsedto.go
+++ models/userrgstatsresponsedto.go
@@ -2,5 +2,5 @@ package models
 
 type UserRgStatsResponseDTO struct {
 	Limits    *[]UserLimitResponseDTO `json:"Limits,omitempty"`
-	PlayStats *PlayerStatsResponseDTO `json:"Limits,omitempty"`
+	PlayStats *PlayerStatsResponseDTO `json:"PlayStats,omitempty"`
 }
