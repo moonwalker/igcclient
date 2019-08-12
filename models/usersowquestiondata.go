e52b881123138fa7515e6dd07e68eff24970b89c
rucsi <rucsi6@gmail.com>
2019-08-12 15:29:47 +0200

    UserSowQuestionData SowAnswerIDs

--- models/usersowquestiondata.go
+++ models/usersowquestiondata.go
@@ -2,6 +2,6 @@ package models
 
 type UserSowQuestionData struct {
 	SowQuestionID *int64   `json:"SowQuestionId,omitempty"`
-	SowAnswerIDs  []*int64 `json:"SowAnswerIds,omitempty"`
+	SowAnswerIDs  *[]int64 `json:"SowAnswerIds,omitempty"`
 	FreeText      *string  `json:"FreeText,omitempty"`
 }
